f1cdac04ef024f357273b87569f47fc6dd54f63f
kaptain.planet <chris.armon20@gmail.com>
2022-06-29 20:07:58 -0500

    added for loop practice file

---
+++ for_loop_practice.py
@@ -0,0 +1,10 @@
+import time # imported the time and sleep() method
+
+# Write a for loop that counts to five.
+for i in range(1,6):
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    print(i ,"Mississippi")
+    # Body of the loop - use: time.sleep(1)
+    time.sleep(1)
+# Write a print function with the final message.
+print("Ready or not, here I come!")
