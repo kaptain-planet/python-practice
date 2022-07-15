bbb392cd1ad395a100ccb45caf334b1975262615
kaptain.planet <chris.armon20@gmail.com>
2022-07-15 08:49:18 -0500

    add index file

---
+++ indexing.py
@@ -0,0 +1,9 @@
+
+numbers = [10, 5, 7, 2, 1]
+print("Original List content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("\nNew List content:", numbers)  # Printing new list content.
+
+numbers[1] = numbers[4] # Copying value of the fifth element to the second.
+print("\nNew List content:", numbers)  # Printing new list content.
