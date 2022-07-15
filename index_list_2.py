c224b43c8a8498632205b713bca0e6a488ce8ee0
kaptain.planet <chris.armon20@gmail.com>
2022-07-15 10:52:47 -0500

    added list

---
+++ index_list_2.py
@@ -0,0 +1,29 @@
+
+# step 1
+beatles = []
+print("Step 1:", beatles)
+
+# step 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+
+# step 3
+for i in range(2):
+    bandmember = str(input("Enter band member's name: "))
+    beatles.append(bandmember)
+print("Step 3:", beatles)
+
+# step 4
+del beatles[4]
+del beatles[3]
+print("Step 4:", beatles)
+
+# step 5
+beatles.insert(0, "Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
